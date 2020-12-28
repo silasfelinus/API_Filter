aa9abaee5d8b76a394a98760bdbfda18d9fc1f02
silasfelinus <silasfelinus@gmail.com>
2020-12-27 22:22:39 -0800

    began initialize code

--- lib/api_filter.rb
+++ lib/api_filter.rb
@@ -4,5 +4,12 @@ require_relative "api_filter/version"
 
 module ApiFilter
   class Error < StandardError; end
-  # Your code goes here...
+  
+  class initialize()
+    puts "Welcome to API_Filter"
+    puts "What would you like to do?"
+
+
+
+
 end
